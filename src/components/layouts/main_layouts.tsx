3573d3d4d3e5c2fadd8a5cfe3a8bf8d38b425bc3
Kruthibharath <kruthi.bharath@gmail.com>
2023-12-13 13:17:04 +0000

    feat: added Routes and outlet component to the application

--- src/components/layouts/main_layouts.tsx
+++ src/components/layouts/main_layouts.tsx
@@ -1,1 +1,13 @@
-export {}
+import { Outlet } from "react-router-dom";
+import Footer from "../footer/footer";
+import Header from "../header/header";
+
+export const MainLayout = () => {
+  <>
+    <Header />
+    <main className="p-6" style={{ flex: "1" }}>
+      <Outlet />
+    </main>
+    <Footer />
+  </>;
+};
--- src/components/router/router.tsx
+++ src/components/router/router.tsx
@@ -1,2 +1,7 @@
-export {}
-// what goes here??
+import { Route, Routes } from "react-router-dom";
+
+export const Router: React.FC = () => {
+  <Routes>
+    <Route path="/" element={<MainLayout />} />
+  </Routes>;
+};
