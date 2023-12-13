e9e3e6233de4210814e9575f1c2ddd3601248f2f
Kruthibharath <kruthi.bharath@gmail.com>
2023-12-13 13:45:32 +0000

    feat: added Navlinks to the navigation

--- src/components/router/router.tsx
+++ src/components/router/router.tsx
@@ -1,16 +1,19 @@
 import { Route, Routes } from "react-router-dom";
+import { MainLayout } from "../layouts/main_layouts";
 import Home from "../home/home";
 import Products from "../products/products";
 import About from "../about/about";
 import Help from "../help/help";
-import { MainLayout } from "../layouts/main_layouts";
 
-export const Router = () => {
-  <Routes>
-    <Route path="/" element={<MainLayout />} />
-    <Route index element={<Home />} />
-    <Route path="/products" element={<Products />} />
-    <Route path="/about" element={<About />} />
-    <Route path="/help" element={<Help />} />
-  </Routes>;
+export const Router: React.FC = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/help" element={<Help />} />
+      </Route>
+    </Routes>
+  );
 };
